09a93ebd2aa030f36e5de78e86c2afaae6eae0a4
Mauricio Roldan <mauricio@localhost.localdomain>
2014-02-28 00:36:32 -0600

    First commit

---
+++ spotifyapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spotifyapp::Application.config.secret_key_base = '93326767de0ec6de908e6a91e72d353e6c2c4ed8a083980fd1378e850b6a39763446ba8d94ba6e774886b48f34b3d5c9ee595a63183ed697c05049c792c049ae'
